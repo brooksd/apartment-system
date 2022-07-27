d0b1bdfdda12f0a6173e82ddfeac4ddd40ba2d20
Donald Brooks Arum <brooksd079@gmail.com>
2022-07-27 10:20:58 +0300

    adds the header component

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = ({ setIsAdding }) => {
+  return (
+    <header>
+        <h1>Aparment Resdents</h1>
+        <div style={{marginTop: '30px', marginBottom: '18px'}}>
+            <button onClick={() => setIsAdding(true)} className='round-bottom'>
+                Add Button
+            </button>
+        </div>
+    </header>
+  )
+}
+
+export default Header;
