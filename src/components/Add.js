f32d7e9a8e55499b6f5fc866dcecf7531d9ed4e0
Donald Brooks Arum <brooksd079@gmail.com>
2022-07-27 12:13:37 +0300

    implements the swal features and also handles the add feature

--- src/components/Add.js
+++ src/components/Add.js
@@ -10,6 +10,30 @@ const Add = ({employees, setEmployees, setIsAdding}) => {
     const [date, setDate] = useState('');
 
     const textInput = useRef(null);
+
+    useEffect(() => {
+        textInput.current.focus();
+    }, [])
+
+    const handleAdd = e => {
+        e.preventDefault();
+        if(!firstName || !lastName || !email || !salary || !date) {
+            return Swal.fire({
+                icon: 'error',
+                title: "Error",
+                text: 'All fields are required',
+                showCancelButton: true
+            });
+        }
+
+        const id = employees.length + 1;
+        const newEmployee = {
+            id, firstName, lastName,
+            email, salary, date
+        }
+
+        employees.push(newEmployee);
+    }
   return (
     <div>Add</div>
   )
