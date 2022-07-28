d58be10b3827f61fa5b41d59061c9562d2c30d75
Donald Brooks Arum <brooksd079@gmail.com>
2022-07-28 11:51:10 +0300

    edit Functionality

--- src/components/Edit.js
+++ src/components/Edit.js
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, {useState} from 'react';
+import Swal from 'sweetalert2';
 
-const Edit = () => {
+const Edit = ({ employees, selectedEmployee, setEmployees, setIsEditing }) => {
+  const id = selectedEmployee.id;
+
+  const [firstName, setFirstName] = useState(selectedEmployee.firstName);
+  const [lastName, setLastName] = useState(selectedEmployee.lastName);
+  const [email, setEmail] = useState(selectedEmployee.email);    const [salary, setSalary] = useState(selectedEmployee.salary);
+  const [date, setDate] = useState(selectedEmployee.date);
+
+  
   return (
     <div>Edit</div>
   )
